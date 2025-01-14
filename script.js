10976d76aa8247624b37e79702ea9141399fecc7
jfogiato <joe.fogiato@gmail.com>
2025-01-14 10:52:48 -0500

    Added function to change image and some basic flex-box styling

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Deployed from GH Pages");
+
+// Global Variables
+const mainImage = document.getElementById("main-image");
+
+function changeMainImage() {
+
+    if (mainImage.src.includes("/Asssets/yahsir_funny_helmet.jpeg")) {
+        mainImage.src = "/Asssets/yahsir_tree.jpeg";
+        mainImage.alt = "An image of Yahsir hanging from a tree while hiking."
+    } else {
+        mainImage.src = "/Asssets/yahsir_funny_helmet.jpeg";
+        mainImage.alt = "An image of Yahsir making a funny face while wearing a bike helmet.";
+    }
+
+};
